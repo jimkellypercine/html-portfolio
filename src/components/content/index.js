4ef0633e874764d5e52ffd22be70358e8936b67f
Jimkelly Percine <jimkellypercine@MacBook-Pro-8.local>
2024-09-29 21:01:46 -0400

    version 2

--- src/components/content/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Title } from './title/Title';
-export { default as Paragraph } from './paragraph/Paragraph';
-export { default as CodeBlock } from './codeBlock/CodeBlock';
--- src/components/home/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from './about/About';
-export { default as Experience } from './experience/Experience';
-export { default as Leadership } from './leadership/Leadership';
-export { default as Projects } from './projects/Projects';
